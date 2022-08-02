5d48f6b0ddac39b2c052a16ced0a752ab95f8ed2
LakshmiPrasannaAdusumalli <prasannacse885@gmail.com>
2022-08-02 16:56:54 +0530

    first commit

---
+++ src/test/java/qatestproject/mavenjava/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qatestproject.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserUIAutomation() {
+		System.out.println("browserUIAutomation");
+	}
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+
+}
